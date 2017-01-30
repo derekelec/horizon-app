a56f009272d9bd5543d197e33923916c250abe68
derekelec <mathewson17@live.ca>
2017-01-30 13:53:16 -0800

    [added] app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// console.log(process.argv);
+var args = process.argv;
+var flagsPresent  = [];
+
+args.forEach(function(item){
+  if(item === '--sum' || item === '--product'){
+    flagsPresent.push(item);
+  }
+})
+
+if(flagsPresent.length !==1) throw 'Error: incorrect number of flags';
+
+var isNumber = function(x){
+  return !isNaN(parseFloat(x));
+}
+var numbers = args.filter(isNumber);
+var result;
+
+if(flagsPresent[0]=== '--sum'){
+  result = numbers.reduce(function(a,b){return a + b;});
+}
+if(flagsPresent[0]=== '--product'){
+  result = numbers.reduce(function(a,b){return a * b;});
+}
+
+console.log("result is %d",result);
